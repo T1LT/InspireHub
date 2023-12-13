c3a8f52f7fd7730ca5086fa9b4f4cf62075d8490
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-13 15:36:47 -0800

    Add delay function for testing data loading

--- src/app/home/todo/page.tsx
+++ src/app/home/todo/page.tsx
@@ -1,7 +1,5 @@
-import TodoCard from "@/components/home/todocard";
 import TodoCardWrapper from "@/components/home/todocardwrapper";
 import { TodosSkeleton } from "@/components/skeletons/todos-skeleton";
-import { todo_data } from "@/lib/todo_data";
 import { Suspense } from "react";
 
 const TodoPage = () => {
--- src/components/home/todocardwrapper.tsx
+++ src/components/home/todocardwrapper.tsx
@@ -1,8 +1,15 @@
 import TodoCard from "./todocard";
 import { Todo, todo_data } from "@/lib/todo_data";
 
+// for testing data loading
+const delayData = async (data: Todo[]) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return data;
+};
+
 export default async function TodoCardWrapper() {
-  const todos = todo_data;
+  // const todos = todo_data;
+  const todos = await delayData(todo_data);
 
   return (
     <div>
