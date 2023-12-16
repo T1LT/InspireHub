3016b5cb2ae6a0e99b819ff13b10c07ac69bd94d
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-16 00:32:46 -0800

    Swap checkbox icons

--- src/components/home/markstatusbutton.tsx
+++ src/components/home/markstatusbutton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaCheckSquare, FaRegSquare } from "react-icons/fa";
+import { SquareIcon, CheckSquare2Icon } from "lucide-react";
 import { Todo } from "@/lib/todo_data";
 import { editTodo } from "@/lib/actions";
 
@@ -21,10 +21,10 @@ export default function MarkStatusButton({
   todo: Todo;
   markAs: "open" | "in_progress" | "completed";
 }) {
-  const Icon = markAs === "completed" ? FaRegSquare : FaCheckSquare;
+  const Icon = markAs === "completed" ? SquareIcon : CheckSquare2Icon;
   return (
     <div className="p-2" onClick={(e) => handleClick(e, todo, markAs)}>
-      <Icon className="text-3xl text-neutral-500 hover:text-blue-600 motion-safe:transition motion-reduce:transition-none" />
+      <Icon className="h-8 w-8 text-neutral-500 hover:text-blue-600 motion-safe:transition motion-reduce:transition-none" />
     </div>
   );
 }
