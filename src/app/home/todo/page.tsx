bcba31cfd296356bd045e1e250f33a80fcacf1b2
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-16 00:20:55 -0800

    Add logos to todo sections

--- src/app/home/todo/page.tsx
+++ src/app/home/todo/page.tsx
@@ -1,5 +1,6 @@
 import TodoCardWrapper from "@/components/home/todocardwrapper";
 import { TodosSkeleton } from "@/components/skeletons/todos-skeleton";
+import { ArrowUpCircle, CheckCircle2, Circle } from "lucide-react";
 import { Suspense } from "react";
 
 const TodoPage = () => {
@@ -7,7 +8,10 @@ const TodoPage = () => {
     <div className="max-h-[calc(100vh-64px)] overflow-x-scroll overscroll-none flex justify-between gap-4 p-4 md:p-8">
       {/* TODOS */}
       <div className="flex flex-col w-full">
-        <h1 className="font-bold text-2xl">Todos</h1>
+        <h1 className="flex items-center font-bold text-2xl mb-2">
+          <Circle className="mr-2 h-5 w-5" />
+          Todos
+        </h1>
         <Suspense fallback={<TodosSkeleton completed={false} />}>
           <TodoCardWrapper status="open" />
         </Suspense>
@@ -15,7 +19,10 @@ const TodoPage = () => {
 
       {/* IN PROGRESS */}
       <div className="flex flex-col w-full">
-        <h1 className="font-bold text-2xl">In Progress</h1>
+        <h1 className="flex items-center font-bold text-2xl mb-2">
+          <ArrowUpCircle className="mr-2 h-5 w-5" />
+          In Progress
+        </h1>
         <Suspense fallback={<TodosSkeleton completed={false} />}>
           <TodoCardWrapper status="in_progress" />
         </Suspense>
@@ -23,7 +30,10 @@ const TodoPage = () => {
 
       {/* DONE */}
       <div className="flex flex-col w-full">
-        <h1 className="font-bold text-2xl">Done</h1>
+        <h1 className="flex items-center font-bold text-2xl mb-2">
+          <CheckCircle2 className="mr-2 h-5 w-5" />
+          Done
+        </h1>
         <Suspense fallback={<TodosSkeleton completed />}>
           <TodoCardWrapper status="completed" />
         </Suspense>
--- src/components/home/status-combobox.tsx
+++ src/components/home/status-combobox.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { ArrowUpCircle, CheckCircle2, Circle, LucideIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
