0de977b5145dbbae05d3fbc2b00b7997d0529407
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-17 04:10:01 -0800

    Add deleteTodo action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,6 +4,21 @@
 import { revalidatePath } from "next/cache";
 import { Todo } from "./todo_data";
 
+export async function createTodo(todoData: Omit<Todo, "todo_id">) {
+  try {
+    await fetch("https://657b6c9d394ca9e4af145532.mockapi.io/api/todos", {
+      method: "POST",
+      body: JSON.stringify(todoData),
+    });
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to create Todo.",
+    };
+  }
+
+  revalidatePath("/home/todo");
+}
+
 export async function editTodo(id: string, newTodo: Todo) {
   try {
     await fetch(`https://657b6c9d394ca9e4af145532.mockapi.io/api/todos/${id}`, {
@@ -17,16 +32,13 @@ export async function editTodo(id: string, newTodo: Todo) {
   revalidatePath("/home/todo");
 }
 
-export async function createTodo(todoData: Omit<Todo, "todo_id">) {
+export async function deleteTodo(id: string) {
   try {
-    await fetch("https://657b6c9d394ca9e4af145532.mockapi.io/api/todos", {
-      method: "POST",
-      body: JSON.stringify(todoData),
+    await fetch(`https://657b6c9d394ca9e4af145532.mockapi.io/api/todos/${id}`, {
+      method: "DELETE",
     });
   } catch (error) {
-    return {
-      message: "Database Error: Failed to create Todo.",
-    };
+    return { message: "Failed to delete Todo." };
   }
 
   revalidatePath("/home/todo");
