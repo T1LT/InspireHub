707308836f99e23ba292f8b7f1477ab8d707562d
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-18 12:43:10 -0800

    Add tooltip to mark status buttons

--- src/components/home/mark-status-button.tsx
+++ src/components/home/mark-status-button.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { Square, CheckSquare2 } from "lucide-react";
 import { Todo } from "@/lib/todo_data";
 import { editTodo } from "@/lib/actions";
+import { Square, CheckSquare2 } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 async function handleClick(
   e: React.MouseEvent<HTMLDivElement, MouseEvent>,
@@ -23,8 +29,21 @@ export default function MarkStatusButton({
 }) {
   const Icon = markAs === "completed" ? Square : CheckSquare2;
   return (
-    <div className="p-2" onClick={(e) => handleClick(e, todo, markAs)}>
-      <Icon className="h-8 w-8 text-neutral-500 hover:text-blue-600 motion-safe:transition motion-reduce:transition-none" />
-    </div>
+    <TooltipProvider>
+      <Tooltip delayDuration={200}>
+        <TooltipTrigger asChild>
+          <div className="p-2" onClick={(e) => handleClick(e, todo, markAs)}>
+            <Icon className="h-8 w-8 text-neutral-500 hover:text-blue-600 motion-safe:transition motion-reduce:transition-none" />
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>
+            {todo.completed === "completed"
+              ? "Mark as Todo"
+              : "Mark as Completed"}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
