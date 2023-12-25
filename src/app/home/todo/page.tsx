eee6e7a014175403621014e46dc3ba58b00af65f
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-25 14:42:23 -0800

    Fix element heights

--- src/app/home/todo/page.tsx
+++ src/app/home/todo/page.tsx
@@ -2,7 +2,7 @@ import TodoList from "@/components/home/todo/todo-list";
 
 const TodoPage = () => {
   return (
-    <div className="max-h-[calc(100vh-64px)] overflow-x-scroll overscroll-none flex justify-between gap-4 p-4 md:p-8">
+    <div className="h-full overflow-x-scroll overscroll-none flex justify-between gap-4 p-4 md:p-8">
       <TodoList title="Todo" status="open" />
       <TodoList title="In Progress" status="in_progress" />
       <TodoList title="Completed" status="completed" />
