15ead9ed659f2b0ef7f56276240c077b94158b5d
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-25 14:55:26 -0800

    Fix filters padding

--- src/components/home/todo/filter-todo.tsx
+++ src/components/home/todo/filter-todo.tsx
@@ -8,7 +8,6 @@ import {
 } from "@/components/ui/popover";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import PriorityBadge from "./priority-label";
-import { Button } from "@/components/ui/button";
 
 interface FilterTodoProps {
   filters: string[] | undefined;
@@ -31,7 +30,7 @@ export default function FilterTodoButton({
               <h4 className="font-semibold leading-none">Filters</h4>
               <button
                 onClick={() => setFilters([])}
-                className="bg-black text-white mr-2 px-2 py-1 text-xs font-medium border rounded-md hover:bg-neutral-800 hover:text-secondary transition"
+                className="bg-black text-white px-2 py-1 text-xs font-medium border rounded-md hover:bg-neutral-800 hover:text-secondary transition"
               >
                 Reset
               </button>
@@ -44,24 +43,24 @@ export default function FilterTodoButton({
               }
               asChild
             >
-              <div className="flex items-center gap-2">
+              <div className="flex items-center justify-between">
                 <h1 className="text-sm font-medium">Priority</h1>
                 <div className="flex gap-1">
                   <ToggleGroupItem
                     value="low"
-                    className="hover:bg-green-100 data-[state=on]:bg-green-200"
+                    className="px-1.5 hover:bg-green-100 data-[state=on]:bg-green-200"
                   >
                     <PriorityBadge priority="low" />
                   </ToggleGroupItem>
                   <ToggleGroupItem
                     value="medium"
-                    className="hover:bg-orange-100 data-[state=on]:bg-orange-200"
+                    className="px-1.5 hover:bg-orange-100 data-[state=on]:bg-orange-200"
                   >
                     <PriorityBadge priority="medium" />
                   </ToggleGroupItem>
                   <ToggleGroupItem
                     value="high"
-                    className="hover:bg-red-100 data-[state=on]:bg-red-200"
+                    className="px-1.5 hover:bg-red-100 data-[state=on]:bg-red-200"
                   >
                     <PriorityBadge priority="high" />
                   </ToggleGroupItem>
